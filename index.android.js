682cd0c6a7e8062789a67d79da0774ca8fec0b68
柯禕藍 <yhilan.ko@gmail.com>
2016-06-22 16:55:34 +1000

    titlebar fixes

--- index.android.js
+++ index.android.js
@@ -11,19 +11,20 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 /* Basis setup */
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableHighlight,
-  TouchableOpacity,
-  Navigator,
-  ListView,
-  ScrollView,
-  Alert,
-  Image
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	TouchableHighlight,
+	TouchableOpacity,
+	Navigator,
+	ListView,
+	ScrollView,
+	Alert,
+	Image,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import YANavigator from 'react-native-ya-navigator';
 
 import * as GLOBAL from './globals.js';
@@ -33,27 +34,34 @@ var dictionary = require('./dictionary.json');
 var searchResults = dictionary;
 
 /* Navigation setup */
-class AhomDict extends React.Component {render() {return (<YANavigator initialRoute={{component: ListPage}}  navBarStyle={{backgroundColor: '#FBC02D'}} />)}}
+class AhomDict extends React.Component {render() {return (
+	<YANavigator initialRoute={{component: ListPage}} 
+	navBarStyle={{backgroundColor: '#FBC02D'}} 
+	navBarBackBtn={{textStyle: {color: '#fff'}}}/>
+)}}
 
 /* The screen that gets seen first */
 class StartPage extends React.Component {
-  render() {
-    return (
-      <YANavigator.Scene
-        delegate={this}
-        style={styles.container}>
-        <Text style={{fontWeight: "bold", fontSize: 18}}>Tai Ahom Dictionary</Text>
-        <Text style={styles.text} onPress = {() => this.props.navigator.push({component: AboutPage, props: {leftBtnText: 'Back'}})}>
-          {'About the Ahom Dictionary'}
-        </Text>
-      </YANavigator.Scene>
-    )
-  }
+	render() {
+		return (
+			<YANavigator.Scene
+			delegate={this}
+			style={styles.container}>
+				<Text style={{fontWeight: "bold", fontSize: 18}}>Tai Ahom Dictionary</Text>
+				<Text style={styles.text} onPress = {() => this.props.navigator.push({component: AboutPage})}>
+				{'About the Ahom Dictionary'}
+				</Text>
+			</YANavigator.Scene>
+		)
+	}
 }
 
 /* Initial list view */
 class ListPage extends React.Component {
-  constructor(props) {
+	renderTitle() {
+		return <Text>Title</Text>;
+	}
+	constructor(props) {
 		 super(props);
 		 this.state = {
 				open: false,
@@ -73,42 +81,43 @@ class ListPage extends React.Component {
 			dataSource: this.state.dataSource.cloneWithRows(this.state.dictionary)
 		})
 	}
-  render() {
-    return (
-      <YANavigator.Scene
-        delegate={this}
-        style={styles.container}>
-          <View style={{flex:1}}>
-          <TextInput ref='search_term' placeholder='Search' onChangeText={(text) => this.setState({text})} onSubmitEditing={this.doSearching.bind(this,this.state.text)}/>
+	render() {
+		return (
+			<YANavigator.Scene
+			delegate={this}
+			style={styles.container}>
+				<View style={{flex:1}}>
+					<TextInput autoCapitalize="none" ref='search_term' placeholder='Search' onChangeText={(text) => this.setState({text})} onSubmitEditing={this.doSearching.bind(this,this.state.text)}/>
 					<ListView
 						dataSource={this.state.dataSource}
 						renderRow={this.renderItem.bind(this)}
 					/>
 				</View>
-      </YANavigator.Scene>
-    )
-  }
-  renderItem(entry,index) {
+			</YANavigator.Scene>
+		)
+	}
+	renderItem(entry,index) {
 		return (
 			<View style={styles.row}>
-        <TouchableOpacity onPress = {this.goToEntry.bind(this,JSON.stringify(entry))}>
-				<Text  style={styles.script}>{entry.lexeme}</Text>
-        <Text style={styles.headword}>{entry.phonemic}</Text>
-        <Text style={styles.snippet}>
-          {entry.pos}
-          {' '}
-          {entry.definition.english}
-				</Text>
-			</TouchableOpacity>
-		</View>
+				<TouchableOpacity onPress = {this.goToEntry.bind(this,JSON.stringify(entry))}>
+					<Text  style={styles.script}>{entry.lexeme}</Text>
+					<Text style={styles.headword}>{entry.phonemic}</Text>
+					<Text style={styles.snippet}>
+						{entry.pos}
+						{' '}
+						{entry.definition.english}
+					</Text>
+				</TouchableOpacity>
+			</View>
 		);
 	}
-  doSearching(term) {
+	doSearching(term) {
 		var numResults = 0;
 		var temparray = [];
-		//GLOBAL.RESULTS;
 		for (var i = 0; i < dictionary.length; i++){
-			if (dictionary[i].phonemic == term) {
+		var line = dictionary[i].definition.english;
+		newterm = new RegExp(term, 'g');
+			if (line.match(newterm)) {
 				temparray.push(dictionary[i]);
 			}
 		}
@@ -122,69 +131,116 @@ class ListPage extends React.Component {
 	}
   goToEntry(entry) {
 		GLOBAL.ENTRY = JSON.parse(entry),
-    this.props.navigator.push({component: EntryPage})
+    this.props.navigator.push({component: EntryPage,props:{backBtnText: 'Back',}})
+	}
+	static navigationDelegate = {
+		renderTitle() {
+			return (
+				<View>
+					<Text style={styles.title}>
+						{'Ahom Dictionary'}
+					</Text>
+				</View>
+			)
+		}
+	}
+}
+
+class View2NavBarTitle extends React.Component {
+	constructor(props) {
+			super(props);
+			this.state = {
+				text: props.text,
+			}
+		}
+		render() {
+			return (
+				<Text style={{fontSize: 18, color: '#fff'}}>AhomDict</Text>
+			)
 	}
 }
 
 /* About page with basic information */
 class AboutPage extends React.Component {
-  render() {
-    return (
-      <YANavigator.Scene
-        delegate={this}
-        style={styles.container}>
-        <Text style={{fontWeight: "bold"}}>AhomDict version 0.1.0</Text>
-        <Text>Data collection & organisation by Poppy Gogoi</Text>
-        <Text>Application development by Kellen Parker van Dam</Text>
-      </YANavigator.Scene>
-    )
-  }
+	render() {
+		return (
+			<YANavigator.Scene
+			delegate={this}
+			style={styles.container}>
+				<Text style={{fontWeight: "bold"}}>AhomDict version 0.1.0</Text>
+				<Text>Data collection & organisation by Poppy Gogoi</Text>
+				<Text>Application development by Kellen Parker van Dam</Text>
+        <Text>(c) 2016 Phonemica</Text>
+        <Text>phonemica.net</Text>
+			</YANavigator.Scene>
+		)
+	}
 }
 
 /* Individual Entries */
 class EntryPage extends React.Component {
-  render() {
-    return (
-      <YANavigator.Scene
-        delegate={this}
-        style={styles.container}>
-        <Text style={styles.script}>{GLOBAL.ENTRY.phonemic}</Text>
-        <Text style={styles.headword}>{GLOBAL.ENTRY.phonemic}</Text>
-        <Text>{GLOBAL.ENTRY.definition.english}</Text>
-      </YANavigator.Scene>
-    )
-  }
+	render() {
+		return (
+			<YANavigator.Scene
+			delegate={this}
+			style={styles.container}>
+				<Text style={styles.script}>{GLOBAL.ENTRY.lexeme}</Text>
+				<Text style={styles.headword}>{GLOBAL.ENTRY.phonemic}</Text>
+				<Text>{GLOBAL.ENTRY.definition.english}</Text>
+			</YANavigator.Scene>
+		)
+	}
+	static navigationDelegate = {
+		renderTitle() {
+			return (
+				<View>
+					<Text style={styles.titleahom}>
+						{GLOBAL.ENTRY.lexeme}
+					</Text>
+				</View>
+			)
+		}
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    backgroundColor: '#F5FCFF',
-    margin: 0,
-    padding: 14
-  },
-  row: {
-	flex: 1,
-	padding: 0,
-	borderBottomWidth: 0.5,
-    borderColor: "#dddddd"
+	container: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+		backgroundColor: '#F5FCFF',
+		margin: 0,
+		padding: 14
 	},
-  headword: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    color: "#333333"
-  },
-  snippet: {
-    fontSize: 16,
-    color: "#333333"
-  },
-  script: {
-    fontSize: 16,
-    fontFamily: 'ahom',
-    color: "#333333"
-  }
+	row: {
+		flex: 1,
+		padding: 0,
+		borderBottomWidth: 0.5,
+		borderColor: "#dddddd"
+	},
+	headword: {
+		fontWeight: 'bold',
+		fontSize: 16,
+		color: "#333333"
+	},
+	snippet: {
+		fontSize: 16,
+		color: "#333333"
+	},
+	script: {
+		fontSize: 16,
+		fontFamily: 'ahom',
+		color: "#333333"
+	},
+	title: {
+		color: '#fff',
+		fontSize: 20
+	},
+	titleahom: {
+		color: '#fff',
+		fontSize: 20,
+		fontFamily: 'ahom'
+	}
 });
 
 AppRegistry.registerComponent('AhomDict', () => AhomDict);
